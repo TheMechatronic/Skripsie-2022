b9d8b2cdd497b899cdf108d015a4b71a0831cc06
TheMechatronic <113066442+TheMechatronic@users.noreply.github.com>
2022-09-07 23:15:42 +0200

    Initial Commit

---
+++ Trainer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Control of a Raspberry Pi based smart trainer developed for Zwift.com connectivity
+
+Serving as GATT server by levraging the built in PyBluez module and dbus.
+"""
+
+__author__ = "DC Eksteen"
+__contact__ = "22623906@sun.ac.za"
+__copyright__ = "NA"
+__credits__ = ["Dr G. Venter", "etc", "etc"]
+__date__ = "2022/09/07"
+__deprecated__ = False
+__email__ =  "gventer@sun.ac.za"
+__maintainer__ = "NA"
+__status__ = "production"
+__version__ = "0.0.1"
